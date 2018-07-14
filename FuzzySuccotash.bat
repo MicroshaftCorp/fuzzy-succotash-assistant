5b73f5b44012a5eddc62dfc823f97972ebbc47a8
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-07-14 10:57:22 -0700

    Update FuzzySuccotash.bat

--- FuzzySuccotash.bat
+++ FuzzySuccotash.bat
@@ -1,5 +1,7 @@
 @echo off
 title Fuzzy Succotash
- ________________________/ O  \___/
- <_O_O_O_O_O_A_O_S_O_O_O_P_____/   \                        
- Start FuzzyTalk.vbs
+ echo ________________________/ O  \___/
+echo <_O_O_O_O_O_A_O_S_O_O_O_P_____/   \                        
+ pause
+ start FuzzyTalk.vbs
+ 
